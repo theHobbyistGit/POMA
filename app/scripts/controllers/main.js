6973a63186018012ed960f32111f35dc6281b7d9
theHobbyistGit <the.hobbyist@gmx.de>
2016-01-26 01:11:58 +0100

    initial commit der boilerplate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pomaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pomaApp
+ */
+angular.module('pomaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
